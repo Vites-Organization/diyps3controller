215f938ac62363c4afbb984b5c13d2443fde4825
mat.lau@laposte.net <mat.lau@laposte.net@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2010-10-22 21:17:25 +0000

    issue 14 multiple sixaxis emulators

--- sixaxis-emu/bt_utils.c
+++ sixaxis-emu/bt_utils.c
@@ -18,6 +18,14 @@ extern int hci_open_dev(int dev_id);
 extern int hci_read_bd_addr(int dd, bdaddr_t *bdaddr, int to);
 extern int hci_write_class_of_dev(int dd, uint32_t cls, int to);
 
+/*
+ * \brief This function gets the bluetooth device address for a given device number.
+ *
+ * \param device_number  the device number
+ * \param bdaddr         the buffer to store the bluetooth device address
+ *
+ * \return 0 if successful, -1 otherwise
+ */
 int get_device_bdaddr(int device_number, char bdaddr[18])
 {
     bdaddr_t bda;
@@ -33,6 +41,13 @@ int get_device_bdaddr(int device_number, char bdaddr[18])
     return 0;
 }
 
+/*
+ * \brief This function writes the bluetooth device class to 0x508 for a given device number.
+ *
+ * \param device_number  the device number
+ *
+ * \return 0 if successful, -1 otherwise
+ */
 int write_device_class(int device_number)
 {
     int s = hci_open_dev (device_number);
