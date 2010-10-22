29d0ee3df69b6987814618d87ccda15d7a509fde
mat.lau@laposte.net <mat.lau@laposte.net@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2010-10-22 20:55:45 +0000

    Replace tabs with spaces

--- sixaxis-emu/sixaxis.h
+++ sixaxis-emu/sixaxis.h
@@ -15,72 +15,72 @@
 #define HID_TYPE_FEATURE 3
 
 struct sixaxis_button {
-	/* It's possible for a button to report as being pressed
+    /* It's possible for a button to report as being pressed
           but still have a zero value */
-	bool pressed;
-	/* 0 to 255 */
-	uint8_t value;
+    bool pressed;
+    /* 0 to 255 */
+    uint8_t value;
 };
 
 struct sixaxis_axis {
-	/* -128 to 127 */
-	int x;
-	int y;
+    /* -128 to 127 */
+    int x;
+    int y;
 };
 
 struct sixaxis_accelerometer {
-	/* -512 to 511 */
-	int x;
-	int y;
-	int z;
-	int gyro;
+    /* -512 to 511 */
+    int x;
+    int y;
+    int z;
+    int gyro;
 };
 
 enum sixaxis_button_index {
-	sb_select = 0, sb_start, sb_ps,
-	sb_up, sb_right, sb_down, sb_left,
-	sb_triangle, sb_circle, sb_cross, sb_square,
-	sb_l1, sb_r1,
-	sb_l2, sb_r2,
-	sb_l3, sb_r3,
-	SB_MAX
+    sb_select = 0, sb_start, sb_ps,
+    sb_up, sb_right, sb_down, sb_left,
+    sb_triangle, sb_circle, sb_cross, sb_square,
+    sb_l1, sb_r1,
+    sb_l2, sb_r2,
+    sb_l3, sb_r3,
+    SB_MAX
 };
 
 enum led_state_t { LED_OFF = 0, LED_FLASH, LED_ON };
 
 struct sixaxis_state_sys {
-	/*** Values provided by the system (PS3): */
-	int reporting_enabled;
-	int feature_ef_byte_6;
+    /*** Values provided by the system (PS3): */
+    int reporting_enabled;
+    int feature_ef_byte_6;
 
-	/* led[0] is the spare */
-	/* led[1]..led[4] correspond to printed text 1..4 */
-	enum led_state_t led[5];
-	uint8_t rumble[2];
+    /* led[0] is the spare */
+    /* led[1]..led[4] correspond to printed text 1..4 */
+    enum led_state_t led[5];
+    uint8_t rumble[2];
 };
 
 struct sixaxis_state_user {
-	/*** Values provided by the user (controller): */
-	struct sixaxis_button button[SB_MAX];
-	struct sixaxis_axis axis[2];
-	struct sixaxis_accelerometer accel;
+    /*** Values provided by the user (controller): */
+    struct sixaxis_button button[SB_MAX];
+    struct sixaxis_axis axis[2];
+    struct sixaxis_accelerometer accel;
 };
 
 struct sixaxis_state {
-	struct sixaxis_state_sys sys;
-	struct sixaxis_state_user user;
+    struct sixaxis_state_sys sys;
+    struct sixaxis_state_user user;
 };
 
 struct sixaxis_assemble_t {
-	int type;
-	uint8_t report;
-	int (*func)(uint8_t *buf, int maxlen, struct sixaxis_state *state);
+    int type;
+    uint8_t report;
+    int (*func)(uint8_t *buf, int maxlen, struct sixaxis_state *state);
 };
 
 struct sixaxis_process_t {
-	int type;
-	uint8_t report;
-	int (*func)(const uint8_t *buf, int len, struct sixaxis_state *state);
+    int type;
+    uint8_t report;
+    int (*func)(const uint8_t *buf, int len, struct sixaxis_state *state);
 };
 
 extern struct sixaxis_assemble_t sixaxis_assemble[];
