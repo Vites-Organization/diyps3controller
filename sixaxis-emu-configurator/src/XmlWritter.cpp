f6f6bc1bb6414f1010349106797817420f342bf6
mathieu.laurendeau@gmail.com <mathieu.laurendeau@gmail.com@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2010-12-20 20:18:51 +0000

    sixaxis-emu-configurator

--- sixaxis-emu-configurator/src/XmlWritter.cpp
+++ sixaxis-emu-configurator/src/XmlWritter.cpp
@@ -19,34 +19,77 @@ XmlWritter::~XmlWritter()
     //dtor
 }
 
-void XmlWritter::CreateEventNode(xmlNodePtr parent_node)
+void XmlWritter::CreateEventNode(xmlNodePtr parent_node, Event* event)
 {
-
+    xmlNodePtr e_node = xmlNewChild(parent_node, NULL, BAD_CAST X_NODE_EVENT, NULL);
+    xmlNewProp(e_node, BAD_CAST X_ATTR_TYPE, BAD_CAST (const char*) event->GetType().mb_str(wxConvUTF8));
+    xmlNewProp(e_node, BAD_CAST X_ATTR_ID, BAD_CAST (const char*) event->GetId().mb_str(wxConvUTF8));
+    xmlNewProp(e_node, BAD_CAST X_ATTR_THRESHOLD, BAD_CAST (const char*) event->GetThreshold().mb_str(wxConvUTF8));
+    if(event->GetType() == _("axis"))
+    {
+        xmlNewProp(e_node, BAD_CAST X_ATTR_DEADZONE, BAD_CAST (const char*) event->GetDeadZone().mb_str(wxConvUTF8));
+        xmlNewProp(e_node, BAD_CAST X_ATTR_MULTIPLIER, BAD_CAST (const char*) event->GetMultiplier().mb_str(wxConvUTF8));
+        xmlNewProp(e_node, BAD_CAST X_ATTR_EXPONENT, BAD_CAST (const char*) event->GetExponent().mb_str(wxConvUTF8));
+    }
 }
 
-void XmlWritter::CreateDeviceNode(xmlNodePtr parent_node)
+void XmlWritter::CreateDeviceNode(xmlNodePtr parent_node, Device* device)
 {
-
+    xmlNodePtr d_node = xmlNewChild(parent_node, NULL, BAD_CAST X_NODE_DEVICE, NULL);
+    xmlNewProp(d_node, BAD_CAST X_ATTR_TYPE, BAD_CAST (const char*) device->GetType().mb_str(wxConvUTF8));
+    xmlNewProp(d_node, BAD_CAST X_ATTR_ID, BAD_CAST (const char*) device->GetId().mb_str(wxConvUTF8));
+    xmlNewProp(d_node, BAD_CAST X_ATTR_NAME, BAD_CAST (const char*) device->GetName().mb_str(wxConvUTF8));
 }
 
 void XmlWritter::CreateAxisMapNode(xmlNodePtr parent_node)
 {
+    xmlNodePtr am_node;
 
+    xmlNodePtr node = xmlNewChild(parent_node, NULL, BAD_CAST X_NODE_AXIS_MAP, NULL);
+    std::list<AxisMapper>* am_list = m_ConfigurationFile->GetController(m_CurrentController)->GetConfiguration(m_CurrentConfiguration)->GetAxisMapperList();
+
+    for(std::list<AxisMapper>::iterator it = am_list->begin(); it!=am_list->end(); it++)
+    {
+        am_node = xmlNewChild(node, NULL, BAD_CAST X_NODE_AXIS, NULL);
+
+        xmlNewProp(am_node, BAD_CAST X_ATTR_ID, BAD_CAST (const char*) it->GetAxis().mb_str(wxConvUTF8));
+
+        CreateDeviceNode(am_node, it->GetDevice());
+
+        CreateEventNode(am_node, it->GetEvent());
+    }
 }
 
 void XmlWritter::CreateButtonMapNode(xmlNodePtr parent_node)
 {
+    xmlNodePtr bm_node;
 
+    xmlNodePtr node = xmlNewChild(parent_node, NULL, BAD_CAST X_NODE_BUTTON_MAP, NULL);
+    std::list<ButtonMapper>* bm_list = m_ConfigurationFile->GetController(m_CurrentController)->GetConfiguration(m_CurrentConfiguration)->GetButtonMapperList();
+
+    for(std::list<ButtonMapper>::iterator it = bm_list->begin(); it!=bm_list->end(); it++)
+    {
+        bm_node = xmlNewChild(node, NULL, BAD_CAST X_NODE_BUTTON, NULL);
+
+        xmlNewProp(bm_node, BAD_CAST X_ATTR_ID, BAD_CAST (const char*) it->GetButton().mb_str(wxConvUTF8));
+
+        CreateDeviceNode(bm_node, it->GetDevice());
+
+        CreateEventNode(bm_node, it->GetEvent());
+    }
 }
 
 void XmlWritter::CreateTriggerNode(xmlNodePtr parent_node)
 {
-    xmlNodePtr node;
     Trigger* trigger = m_ConfigurationFile->GetController(m_CurrentController)->GetConfiguration(m_CurrentConfiguration)->GetTrigger();
 
-    node = xmlNewChild(parent_node, NULL, BAD_CAST X_NODE_TRIGGER, NULL);
+    xmlNodePtr node = xmlNewChild(parent_node, NULL, BAD_CAST X_NODE_TRIGGER, NULL);
 
-    //xmlNewProp(node, BAD_CAST X_ATTR_DEVICE_TYPE, BAD_CAST trigger->GetDevice()->GetType().mb_str());
+    xmlNewProp(node, BAD_CAST X_ATTR_TYPE, BAD_CAST (const char*) trigger->GetDevice()->GetType().mb_str(wxConvUTF8));
+
+    xmlNewProp(node, BAD_CAST X_ATTR_ID, BAD_CAST (const char*) trigger->GetDevice()->GetId().mb_str(wxConvUTF8));
+
+    xmlNewProp(node, BAD_CAST X_ATTR_BUTTON_ID, BAD_CAST (const char*) trigger->GetEvent()->GetId().mb_str(wxConvUTF8));
 }
 
 void XmlWritter::CreateConfigurationNodes(xmlNodePtr parent_node)
@@ -63,7 +106,7 @@ void XmlWritter::CreateConfigurationNodes(xmlNodePtr parent_node)
 
         snprintf(id, sizeof(id), "%hhu", i);
 
-        xmlNewProp(node, BAD_CAST "id", BAD_CAST id);
+        xmlNewProp(node, BAD_CAST X_ATTR_ID, BAD_CAST id);
 
         CreateTriggerNode(node);
 
@@ -87,7 +130,7 @@ void XmlWritter::CreateControllerNodes(xmlNodePtr parent_node)
 
         snprintf(id, sizeof(id), "%hhu", i);
 
-        xmlNewProp(node, BAD_CAST "id", BAD_CAST id);
+        xmlNewProp(node, BAD_CAST X_ATTR_ID, BAD_CAST id);
 
         CreateConfigurationNodes(node);
     }
@@ -96,9 +139,7 @@ void XmlWritter::CreateControllerNodes(xmlNodePtr parent_node)
 void XmlWritter::WriteConfigFile()
 {
     xmlDocPtr doc = NULL;       /* document pointer */
-    xmlNodePtr root_node = NULL, node = NULL, node1 = NULL;/* node pointers */
-    char buff[256];
-    int i, j;
+    xmlNodePtr root_node = NULL;/* node pointers */
 
     LIBXML_TEST_VERSION;
 
