2b130898f422faafe27f39b072403aeca95a4df2
mathieu.laurendeau@gmail.com <mathieu.laurendeau@gmail.com@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2011-06-01 17:48:08 +0000

    Code cleaning

--- sixaxis-emu/win_serial_con.c
+++ sixaxis-emu/win_serial_con.c
@@ -11,6 +11,7 @@
 #include <windows.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include "config.h"
 
 /*
  * The usb report structure to send over the serial connection.
@@ -42,6 +43,9 @@ static int debug;
  */
 extern int max_axis_value;
 extern int mean_axis_value;
+
+extern s_controller controller[MAX_CONTROLLERS];
+extern struct sixaxis_state state[MAX_CONTROLLERS];
 
 /*
  * Connect to a serial port.
@@ -87,7 +91,7 @@ int serial_connect(char* portname)
 /*
  * Send a usb report to the serial port.
  */
-int serial_send(struct sixaxis_state *state)
+int serial_send()
 {
   s_report_data data =
   { };
@@ -99,68 +103,68 @@ int serial_send(struct sixaxis_state *state)
   /*
    * Make sure the value is not out of range.
    */
-  data.X = clamp(0, state->user.axis[0][0] + mean_axis_value, max_axis_value);
-  data.Y = clamp(0, state->user.axis[0][1] + mean_axis_value, max_axis_value);
-  data.Z = clamp(0, state->user.axis[1][0] + mean_axis_value, max_axis_value);
-  data.Rz = clamp(0, state->user.axis[1][1] + mean_axis_value, max_axis_value);
-  if (state->user.button[sb_square].pressed)
+  data.X = clamp(0, state[0].user.axis[0][0] + mean_axis_value, max_axis_value);
+  data.Y = clamp(0, state[0].user.axis[0][1] + mean_axis_value, max_axis_value);
+  data.Z = clamp(0, state[0].user.axis[1][0] + mean_axis_value, max_axis_value);
+  data.Rz = clamp(0, state[0].user.axis[1][1] + mean_axis_value, max_axis_value);
+  if (state[0].user.button[sb_square].pressed)
   {
     data.Bt |= 0x0001;
   }
-  if (state->user.button[sb_cross].pressed)
+  if (state[0].user.button[sb_cross].pressed)
   {
     data.Bt |= 0x0002;
   }
-  if (state->user.button[sb_circle].pressed)
+  if (state[0].user.button[sb_circle].pressed)
   {
     data.Bt |= 0x0004;
   }
-  if (state->user.button[sb_triangle].pressed)
+  if (state[0].user.button[sb_triangle].pressed)
   {
     data.Bt |= 0x0008;
   }
 
-  if (state->user.button[sb_select].pressed)
+  if (state[0].user.button[sb_select].pressed)
   {
     data.Bt |= 0x0100;
   }
-  if (state->user.button[sb_start].pressed)
+  if (state[0].user.button[sb_start].pressed)
   {
     data.Bt |= 0x0200;
   }
-  if (state->user.button[sb_l3].pressed)
+  if (state[0].user.button[sb_l3].pressed)
   {
     data.Bt |= 0x0400;
   }
-  if (state->user.button[sb_r3].pressed)
+  if (state[0].user.button[sb_r3].pressed)
   {
     data.Bt |= 0x0800;
   }
 
-  if (state->user.button[sb_l1].pressed)
+  if (state[0].user.button[sb_l1].pressed)
   {
     data.Bt |= 0x0010;
   }
-  if (state->user.button[sb_r1].pressed)
+  if (state[0].user.button[sb_r1].pressed)
   {
     data.Bt |= 0x0020;
   }
-  if (state->user.button[sb_l2].pressed)
+  if (state[0].user.button[sb_l2].pressed)
   {
     data.Bt |= 0x0040;
   }
-  if (state->user.button[sb_r2].pressed)
+  if (state[0].user.button[sb_r2].pressed)
   {
     data.Bt |= 0x0080;
   }
 
-  if (state->user.button[sb_right].pressed)
+  if (state[0].user.button[sb_right].pressed)
   {
-    if (state->user.button[sb_down].pressed)
+    if (state[0].user.button[sb_down].pressed)
     {
       data.Hat = 0x0003;
     }
-    else if (state->user.button[sb_up].pressed)
+    else if (state[0].user.button[sb_up].pressed)
     {
       data.Hat = 0x0001;
     }
@@ -169,13 +173,13 @@ int serial_send(struct sixaxis_state *state)
       data.Hat = 0x0002;
     }
   }
-  else if (state->user.button[sb_left].pressed)
+  else if (state[0].user.button[sb_left].pressed)
   {
-    if (state->user.button[sb_down].pressed)
+    if (state[0].user.button[sb_down].pressed)
     {
       data.Hat = 0x0005;
     }
-    else if (state->user.button[sb_up].pressed)
+    else if (state[0].user.button[sb_up].pressed)
     {
       data.Hat = 0x0007;
     }
@@ -184,11 +188,11 @@ int serial_send(struct sixaxis_state *state)
       data.Hat = 0x0006;
     }
   }
-  else if (state->user.button[sb_down].pressed)
+  else if (state[0].user.button[sb_down].pressed)
   {
     data.Hat = 0x0004;
   }
-  else if (state->user.button[sb_up].pressed)
+  else if (state[0].user.button[sb_up].pressed)
   {
     data.Hat = 0x0000;
   }
