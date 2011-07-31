5546b909e643bd2f1916e89894209915c6c84876
mat.lau@laposte.net <mat.lau@laposte.net@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2011-07-31 15:11:30 +0000

    issue 110

--- sixaxis-emu-configurator/include/Trigger.h
+++ sixaxis-emu-configurator/include/Trigger.h
@@ -8,7 +8,7 @@ class Trigger
 {
     public:
         Trigger();
-        Trigger(wxString dtype, wxString did, wxString dname, wxString eid, wxString switchback);
+        Trigger(wxString dtype, wxString did, wxString dname, wxString eid, wxString switchback, unsigned short delay);
         virtual ~Trigger();
         Trigger(const Trigger& other);
         Trigger& operator=(const Trigger& other);
@@ -18,11 +18,14 @@ class Trigger
         void SetEvent(Event val) { m_Event = val; }
         wxString GetSwitchBack() { return m_SwitchBack; }
         void SetSwitchBack(wxString val) { m_SwitchBack = val; }
+        unsigned short GetDelay() { return m_Delay; }
+        void SetDelay(unsigned short val) { m_Delay = val; }
     protected:
     private:
         Device m_Device;
         Event m_Event;
         wxString m_SwitchBack;
+        unsigned short m_Delay;
 };
 
 #endif // TRIGGER_H
