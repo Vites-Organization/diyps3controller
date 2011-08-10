b5b7002d7e37d38e93add463d9ffb2cb9ab42b9c
mat.lau@laposte.net <mat.lau@laposte.net@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2011-08-10 20:55:32 +0000

    issue 94

--- fpsconfig/fpsconfigMain.cpp
+++ fpsconfig/fpsconfigMain.cpp
@@ -167,19 +167,20 @@ fpsconfigFrame::fpsconfigFrame(wxWindow* parent,wxWindowID id)
 
     Create(parent, wxID_ANY, _("fpsconfig"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("wxID_ANY"));
     SetClientSize(wxSize(620,410));
+    SetBackgroundColour(wxColour(255,255,255));
     Panel1 = new wxPanel(this, ID_PANEL1, wxPoint(0,0), wxSize(620,372), wxTAB_TRAVERSAL, _T("ID_PANEL1"));
     Panel1->SetBackgroundColour(wxColour(255,255,255));
-    SpinCtrl8 = new wxSpinCtrl(Panel1, ID_SPINCTRL8, _T("100"), wxPoint(592,352), wxSize(20,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL8"));
+    SpinCtrl8 = new wxSpinCtrl(Panel1, ID_SPINCTRL8, _T("100"), wxPoint(592,352), wxSize(19,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL8"));
     SpinCtrl8->SetValue(_T("100"));
-    SpinCtrl7 = new wxSpinCtrl(Panel1, ID_SPINCTRL7, _T("100"), wxPoint(592,320), wxSize(20,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL7"));
+    SpinCtrl7 = new wxSpinCtrl(Panel1, ID_SPINCTRL7, _T("100"), wxPoint(592,320), wxSize(19,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL7"));
     SpinCtrl7->SetValue(_T("100"));
-    SpinCtrl6 = new wxSpinCtrl(Panel1, ID_SPINCTRL6, _T("100"), wxPoint(496,352), wxSize(20,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL6"));
+    SpinCtrl6 = new wxSpinCtrl(Panel1, ID_SPINCTRL6, _T("100"), wxPoint(496,352), wxSize(19,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL6"));
     SpinCtrl6->SetValue(_T("100"));
-    SpinCtrl5 = new wxSpinCtrl(Panel1, ID_SPINCTRL5, _T("100"), wxPoint(496,320), wxSize(20,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL5"));
+    SpinCtrl5 = new wxSpinCtrl(Panel1, ID_SPINCTRL5, _T("100"), wxPoint(496,320), wxSize(19,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL5"));
     SpinCtrl5->SetValue(_T("100"));
-    SpinCtrl4 = new wxSpinCtrl(Panel1, ID_SPINCTRL4, _T("100"), wxPoint(400,352), wxSize(20,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL4"));
+    SpinCtrl4 = new wxSpinCtrl(Panel1, ID_SPINCTRL4, _T("100"), wxPoint(400,352), wxSize(19,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL4"));
     SpinCtrl4->SetValue(_T("100"));
-    SpinCtrl3 = new wxSpinCtrl(Panel1, ID_SPINCTRL3, _T("100"), wxPoint(400,320), wxSize(20,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL3"));
+    SpinCtrl3 = new wxSpinCtrl(Panel1, ID_SPINCTRL3, _T("100"), wxPoint(400,320), wxSize(19,-1), 0, -10000, 10000, 100, _T("ID_SPINCTRL3"));
     SpinCtrl3->SetValue(_T("100"));
     stickright = new wxButton(Panel1, ID_BUTTON10, wxEmptyString, wxPoint(256,222), wxSize(50,-1), 0, wxDefaultValidator, _T("ID_BUTTON10"));
     wxFont stickrightFont(6,wxDEFAULT,wxFONTSTYLE_NORMAL,wxNORMAL,false,wxEmptyString,wxFONTENCODING_DEFAULT);
