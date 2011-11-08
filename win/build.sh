dcb1df91a9436a11888e3a7c58add7fd33f2ea66
mathieu.laurendeau@gmail.com <mathieu.laurendeau@gmail.com@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2011-11-08 15:08:26 +0000

    Small improvement.

--- win/build.sh
+++ win/build.sh
@@ -12,10 +12,8 @@ SVN_VERSION=1.6.17
 #install svn
 if ! test -f /usr/local/bin/svn.exe
 then
-  wget http://sourceforge.net/projects/win32svn/files/$SVN_VERSION/svn-win32-$SVN_VERSION.zip/download
-  unzip svn-win32-$SVN_VERSION.zip
-  mkdir -p /usr/local/bin
-  mv svn-win32-$SVN_VERSION/bin/* /usr/local/bin/
+  wget http://www.gimx.fr/archive/svn-win32-$SVN_VERSION.zip
+  unzip -u -d /usr/local/ svn-win32-$SVN_VERSION.zip
   rm -rf svn-win32-$SVN_VERSION svn-win32-$SVN_VERSION.zip
 fi
 
