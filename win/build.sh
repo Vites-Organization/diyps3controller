b577af9bfa905d54dd6b5681123985d1cecf41d1
mathieu.laurendeau@gmail.com <mathieu.laurendeau@gmail.com@02dc42d0-1a0e-11df-85e6-9b4bd7dd5138>
2011-11-17 09:50:35 +0000

    Creation of setup folder moved into the makefile.

--- win/build.sh
+++ win/build.sh
@@ -32,28 +32,5 @@ then
 fi
 
 cd GIMX
-make -f Makefile.win -j $CPU
-
-#copy GIMX binaries & config file
-if ! test -d setup
-then
-  mkdir -p setup
-  mkdir -p setup/config
-  mkdir -p setup/macros
-  cp $PREFIX/bin/libgcc_s_dw2-1.dll setup
-  cp $PREFIX/bin/libiconv-2.dll setup
-  cp $PREFIX/bin/libpthread-2.dll setup
-  cp $PREFIX/bin/libstdc++-6.dll setup
-  cp $PREFIX/bin/libxml2.dll setup
-  cp $PREFIX/bin/SDL.dll setup
-  cp $PREFIX/bin/zlib1.dll setup
-fi
-
-cp sixaxis-emu/emuclient.exe setup
-cp -r sixaxis-emu/config/*.xml setup/config
-cp -r sixaxis-emu/macros/*.txt setup/macros
-cp sixaxis-emu-configurator/bin/Release/gimx-config.exe setup
-cp sixemugui-serial/bin/Release/gimx-serial.exe setup
-cp sixstatus/bin/Release/gimx-status.exe setup
-cp fpsconfig/bin/Release/gimx-fpsconfig.exe setup
+make -f Makefile.win -j $CPU install
 
